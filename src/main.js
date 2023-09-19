65122be27f10c457c0c89e8c553f0b8de3281740
Ts-Dmitriy <112119145+Ts-Dmitriy@users.noreply.github.com>
2023-09-19 14:44:46 +0600

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from '../store'
-import router from './router/index'
-createApp(App).mount('#app')
-App.use(router)
-App.use(store)
-App.use(router)
